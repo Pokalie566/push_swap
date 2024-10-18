c202594d96d1e696e08ed153429ef8aec82d4fb1
Pokalie566 <48855608+Pokalie566@users.noreply.github.com>
2024-10-18 19:59:32 +0200

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -1,22 +1,99 @@
-void fusion (int *a, int n, int m) {
-    int i, j, k;
-    int *x = malloc(n * sizeof (int));
-    for (i = 0, j = m, k = 0; k < n; k++) {
-        x[k] = j == n      ? a[i++]
-             : i == m      ? a[j++]
-             : a[j] < a[i] ? a[j++]
-             :               a[i++];
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 19:26:33 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/18 19:26:33 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    int* data;
+    int size;
+    int capacity;
+} Pile;
+
+void initPile(Pile* pile, int n) {
+    pile->data = (int*)malloc(n * sizeof(int));
+    pile->size = n;
+    pile->capacity = n;
+
+    srand(time(NULL)); 
+
+    for (int i = 0; i < n; i++)
+    {
+        pile->data[i] = rand() % 100; 
     }
-    for (i = 0; i < n; i++) {
-        a[i] = x[i];
-    }
-    free(x);
 }
- 
-void tri_fusion (int *liste, int taille) {
-    if (taille < 2) return;
-    int milieu = taille / 2;
-    tri_fusion(liste, milieu);
-    tri_fusion(liste milieu, taille - milieu);
-    fusion(liste, taille, milieu);
+
+int* merge(int* left, int sizeLeft, int* right, int sizeRight)
+{
+    int* result = (int*)malloc((sizeLeft + sizeRight) * sizeof(int));
+    int i = 0, j = 0, k = 0;
+
+    while (i < sizeLeft && j < sizeRight) {
+        if (left[i] <= right[j]) {
+            result[k++] = left[i++];
+        } else {
+            result[k++] = right[j++];
+        }
+    }
+    while (i < sizeLeft) {
+        result[k++] = left[i++];
+    }
+
+    while (j < sizeRight) {
+        result[k++] = right[j++];
+    }
+
+    return result;
+}
+
+void triFusion(Pile* pile) {
+    if (pile->size <= 1) {
+        return;
+    }
+
+    int mid = pile->size / 2;
+    Pile left = {pile->data, mid, mid};
+    Pile right = {pile->data + mid, pile->size - mid, pile->size - mid};
+
+    triFusion(&left);
+    triFusion(&right);
+
+    int* mergedData = merge(left.data, left.size, right.data, right.size);
+
+    for (int i = 0; i < pile->size; i++) {
+        pile->data[i] = mergedData[i];
+    }
+    free(mergedData);
+}
+
+int main() {
+    Pile pile;
+    initPile(&pile, 1000); 
+
+    printf("Pile avant tri : ");
+    for (int i = 0; i < pile.size; i++) {
+        printf("%d ", pile.data[i]);
+    }
+    printf("\n");
+
+    triFusion(&pile);
+
+    printf("Pile après tri : ");
+    for (int i = 0; i < pile.size; i++) {
+        printf("%d ", pile.data[i]);
+    }
+    printf("\n");
+
+    free(pile.data);
+    return 0;
 }
